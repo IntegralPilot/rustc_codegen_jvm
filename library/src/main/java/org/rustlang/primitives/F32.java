340373ed112d60699f390dff003a8dd1af83787d
Michael Reeves <michael.reeves077@gmail.com>
2026-07-15 10:03:29 +1000

    rm shim stuff no longer needed

--- library/src/main/java/org/rustlang/primitives/F32.java
+++
@@ -1,9 +0,0 @@
-package org.rustlang.primitives;
-
-public final class F32 {
-    private F32() {}
-
-    public static boolean eq(float a, float b) {
-        return a == b;
-    }
-}
--- library/src/main/java/org/rustlang/primitives/F64.java
+++
@@ -1,9 +0,0 @@
-package org.rustlang.primitives;
-
-public final class F64 {
-    private F64() {}
-
-    public static boolean eq(double a, double b) {
-        return a == b;
-    }
-}
